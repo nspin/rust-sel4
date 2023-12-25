aa926258649285f75f2ecdf71e582c60b3118f22
Nick Spinale <nick@nickspinale.com>
2023-12-25 03:01:42 -0500

    x

--- hacking/docker/setup.sh
+++ hacking/docker/setup.sh
@@ -9,7 +9,7 @@ set -eu -o pipefail
 if [ ! -f /nix/.installed ]; then
     curl -L https://nixos.org/nix/install | \
         sh -s -- --yes --no-channel-add --no-modify-profile
-    ln -s $(readlink --canonicalize-existing $HOME/.nix-profile) /nix/var/nix/env
-    rm -r $HOME/.nix-profile $HOME/.nix-defexpr $HOME/.local/state/nix
+    # ln -s $(readlink --canonicalize-existing $HOME/.nix-profile) /nix/var/nix/env
+    # rm -r $HOME/.nix-profile $HOME/.nix-defexpr $HOME/.local/state/nix
     touch /nix/.installed
 fi
