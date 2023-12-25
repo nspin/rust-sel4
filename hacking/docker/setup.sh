6e53c1991be68753995ee2b13e18ad8419c5a61e
Nick Spinale <nick@nickspinale.com>
2023-12-25 01:21:56 -0500

    Rework Docker setup

--- hacking/docker/setup.sh
+++ hacking/docker/setup.sh
@@ -4,12 +4,12 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
-set -e
+set -eu -o pipefail
 
 if [ ! -f /nix/.installed ]; then
     curl -L https://nixos.org/nix/install | \
         sh -s -- --yes --no-channel-add --no-modify-profile
-    ln -s $(readlink --canonicalize $HOME/.nix-profile) /nix/var/nix/the-profile
+    ln -s $(readlink --canonicalize-existing $HOME/.nix-profile) /nix/var/nix/env
     rm -r $HOME/.nix-profile $HOME/.nix-defexpr $HOME/.local/state/nix
     touch /nix/.installed
 fi
