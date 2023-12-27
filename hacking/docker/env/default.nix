79083387341ea8ebde9071d66ef5f045d862f8eb
Nick Spinale <nick@nickspinale.com>
2023-12-27 03:35:54 -0500

    x

--- hacking/docker/env/default.nix
+++ hacking/docker/env/default.nix
@@ -26,12 +26,8 @@ let
     configuration = ./config;
   };
 
-  # image = pkgs.callPackage ./image.nix {};
-
 in {
   inherit home;
   inherit (home) activationPackage;
   inherit (home.config.home) path;
-
-  # inherit image;
 }
