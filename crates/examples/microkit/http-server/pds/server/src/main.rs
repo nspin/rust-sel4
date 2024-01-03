5d4d3077d4f5368b46d2c6067eaf6c6ab513e670
Nick Spinale <nick@nickspinale.com>
2024-01-03 11:43:30 +0000

    x

--- crates/examples/microkit/http-server/pds/server/src/main.rs
+++ crates/examples/microkit/http-server/pds/server/src/main.rs
@@ -239,6 +239,7 @@ mod rand_env {
     getrandom::register_custom_getrandom!(insecure_dummy_rng);
 
     // https://github.com/rust-lang/compiler-builtins/pull/563
+    #[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
     #[no_mangle]
     pub extern "C" fn __bswapsi2(u: u32) -> u32 {
         ((u & 0xff000000) >> 24)
