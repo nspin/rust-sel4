d77cf541858b6adefda0597bd5637d7e6376e2ed
Nick Spinale <nick@nickspinale.com>
2024-06-17 17:04:01 -0400

    x

--- crates/sel4-driver-interfaces/src/lib.rs
+++ crates/sel4-driver-interfaces/src/lib.rs
@@ -6,8 +6,9 @@
 
 #![no_std]
 
+use core::cell::{RefCell, RefMut};
 use core::fmt;
-// use core::cell::BorrowMutError;
+use core::ops::Deref;
 
 pub mod block;
 pub mod net;
@@ -19,27 +20,33 @@ pub trait HandleInterrupt {
 }
 
 #[derive(Debug, Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Hash, Default)]
-pub struct Shared<T>(pub T);
+pub struct WrappedRefCell<T>(pub T);
+
+impl<T> WrappedRefCell<T> {
+    fn try_borrow_mut<E, U>(&self) -> Result<RefMut<U>, WrappedRefCellError<E>>
+    where
+        T: Deref<Target = RefCell<U>>,
+    {
+        self.0
+            .deref()
+            .try_borrow_mut()
+            .map_err(|_| WrappedRefCellError::Contention)
+    }
+}
 
 #[derive(Debug, Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Hash)]
-pub enum SharedRefCellError<E> {
+pub enum WrappedRefCellError<E> {
     Contention,
     Other(E),
 }
 
-// impl<E> From<BorrowMutError> for SharedRefCellError<E> {
-//     fn from(_: BorrowMutError) -> Self {
-//         Self::Contention
-//     }
-// }
+impl<E> From<E> for WrappedRefCellError<E> {
+    fn from(err: E) -> Self {
+        Self::Other(err)
+    }
+}
 
-// impl<E> From<E> for SharedRefCellError<E> {
-//     fn from(err: E) -> Self {
-//         Self::Other(err)
-//     }
-// }
-
-impl<E: fmt::Display> fmt::Display for SharedRefCellError<E> {
+impl<E: fmt::Display> fmt::Display for WrappedRefCellError<E> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::Contention => write!(f, "contention"),
@@ -47,3 +54,6 @@ impl<E: fmt::Display> fmt::Display for SharedRefCellError<E> {
         }
     }
 }
+
+#[derive(Debug, Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Hash, Default)]
+pub struct WrappedMutex<T>(pub T);
--- crates/sel4-driver-interfaces/src/serial/mod.rs
+++ crates/sel4-driver-interfaces/src/serial/mod.rs
@@ -9,14 +9,15 @@ use core::ops::Deref;
 
 use embedded_hal_nb::nb;
 use embedded_hal_nb::serial::{Error, ErrorKind, ErrorType, Read, Write};
+use lock_api::{Mutex, RawMutex};
 
-use crate::{Shared, SharedRefCellError};
+use crate::{WrappedMutex, WrappedRefCell, WrappedRefCellError};
 
 mod write_buffered;
 
 pub use write_buffered::WriteBuffered;
 
-impl<E: Error> Error for SharedRefCellError<E> {
+impl<E: Error> Error for WrappedRefCellError<E> {
     fn kind(&self) -> ErrorKind {
         match self {
             Self::Contention => ErrorKind::Other,
@@ -25,37 +26,52 @@ impl<E: Error> Error for SharedRefCellError<E> {
     }
 }
 
-impl<T: Deref<Target = RefCell<U>>, U: ErrorType> ErrorType for Shared<T> {
-    type Error = SharedRefCellError<<U as ErrorType>::Error>;
+impl<T: Deref<Target = RefCell<U>>, U: ErrorType> ErrorType for WrappedRefCell<T> {
+    type Error = WrappedRefCellError<<U as ErrorType>::Error>;
 }
 
-impl<Word: Copy, T: Deref<Target = RefCell<U>>, U: Read<Word>> Read<Word> for Shared<T> {
+impl<Word: Copy, T: Deref<Target = RefCell<U>>, U: Read<Word>> Read<Word> for WrappedRefCell<T> {
     fn read(&mut self) -> nb::Result<Word, Self::Error> {
-        self.0
-            .deref()
-            .try_borrow_mut()
-            .map_err(|_| SharedRefCellError::Contention)?
+        self.try_borrow_mut()?
             .read()
-            .map_err(|err| err.map(SharedRefCellError::Other))
+            .map_err(|err| err.map(WrappedRefCellError::Other))
     }
 }
 
-impl<Word: Copy, T: Deref<Target = RefCell<U>>, U: Write<Word>> Write<Word> for Shared<T> {
+impl<Word: Copy, T: Deref<Target = RefCell<U>>, U: Write<Word>> Write<Word> for WrappedRefCell<T> {
     fn write(&mut self, word: Word) -> nb::Result<(), Self::Error> {
-        self.0
-            .deref()
-            .try_borrow_mut()
-            .map_err(|_| SharedRefCellError::Contention)?
+        self.try_borrow_mut()?
             .write(word)
-            .map_err(|err| err.map(SharedRefCellError::Other))
+            .map_err(|err| err.map(WrappedRefCellError::Other))
     }
 
     fn flush(&mut self) -> nb::Result<(), Self::Error> {
-        self.0
-            .deref()
-            .try_borrow_mut()
-            .map_err(|_| SharedRefCellError::Contention)?
+        self.try_borrow_mut()?
             .flush()
-            .map_err(|err| err.map(SharedRefCellError::Other))
+            .map_err(|err| err.map(WrappedRefCellError::Other))
+    }
+}
+
+impl<R: RawMutex, T: Deref<Target = Mutex<R, U>>, U: ErrorType> ErrorType for WrappedMutex<T> {
+    type Error = <U as ErrorType>::Error;
+}
+
+impl<R: RawMutex, Word: Copy, T: Deref<Target = Mutex<R, U>>, U: Read<Word>> Read<Word>
+    for WrappedMutex<T>
+{
+    fn read(&mut self) -> nb::Result<Word, Self::Error> {
+        self.0.deref().lock().read()
+    }
+}
+
+impl<R: RawMutex, Word: Copy, T: Deref<Target = Mutex<R, U>>, U: Write<Word>> Write<Word>
+    for WrappedMutex<T>
+{
+    fn write(&mut self, word: Word) -> nb::Result<(), Self::Error> {
+        self.0.deref().lock().write(word)
+    }
+
+    fn flush(&mut self) -> nb::Result<(), Self::Error> {
+        self.0.deref().lock().flush()
     }
 }
