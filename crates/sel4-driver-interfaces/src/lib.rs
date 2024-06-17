0c5d5ae2af19fcc3eb61364814337b61f8c4a65d
Nick Spinale <nick@nickspinale.com>
2024-06-17 16:49:29 -0400

    x

--- crates/sel4-driver-interfaces/src/lib.rs
+++ crates/sel4-driver-interfaces/src/lib.rs
@@ -6,14 +6,44 @@
 
 #![no_std]
 
+use core::fmt;
+// use core::cell::BorrowMutError;
+
 pub mod block;
 pub mod net;
 pub mod serial;
 pub mod timer;
 
+pub trait HandleInterrupt {
+    fn handle_interrupt(&mut self);
+}
+
 #[derive(Debug, Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Hash, Default)]
 pub struct Shared<T>(pub T);
 
-pub trait HandleInterrupt {
-    fn handle_interrupt(&mut self);
+#[derive(Debug, Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Hash)]
+pub enum SharedRefCellError<E> {
+    Contention,
+    Other(E),
+}
+
+// impl<E> From<BorrowMutError> for SharedRefCellError<E> {
+//     fn from(_: BorrowMutError) -> Self {
+//         Self::Contention
+//     }
+// }
+
+// impl<E> From<E> for SharedRefCellError<E> {
+//     fn from(err: E) -> Self {
+//         Self::Other(err)
+//     }
+// }
+
+impl<E: fmt::Display> fmt::Display for SharedRefCellError<E> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Contention => write!(f, "contention"),
+            Self::Other(err) => err.fmt(f),
+        }
+    }
 }
--- crates/sel4-driver-interfaces/src/serial/mod.rs
+++ crates/sel4-driver-interfaces/src/serial/mod.rs
@@ -4,17 +4,58 @@
 // SPDX-License-Identifier: BSD-2-Clause
 //
 
+use core::cell::RefCell;
 use core::ops::Deref;
 
 use embedded_hal_nb::nb;
-use embedded_hal_nb::serial::{ErrorType, Read, Write};
+use embedded_hal_nb::serial::{Error, ErrorKind, ErrorType, Read, Write};
 
-use crate::Shared;
+use crate::{Shared, SharedRefCellError};
 
 mod write_buffered;
 
 pub use write_buffered::WriteBuffered;
 
-impl<T: Deref<Target: ErrorType>> ErrorType for Shared<T> {
-    type Error = T::Target::Error;
+impl<E: Error> Error for SharedRefCellError<E> {
+    fn kind(&self) -> ErrorKind {
+        match self {
+            Self::Contention => ErrorKind::Other,
+            Self::Other(err) => err.kind(),
+        }
+    }
+}
+
+impl<T: Deref<Target = RefCell<U>>, U: ErrorType> ErrorType for Shared<T> {
+    type Error = SharedRefCellError<<U as ErrorType>::Error>;
+}
+
+impl<Word: Copy, T: Deref<Target = RefCell<U>>, U: Read<Word>> Read<Word> for Shared<T> {
+    fn read(&mut self) -> nb::Result<Word, Self::Error> {
+        self.0
+            .deref()
+            .try_borrow_mut()
+            .map_err(|_| SharedRefCellError::Contention)?
+            .read()
+            .map_err(|err| err.map(SharedRefCellError::Other))
+    }
+}
+
+impl<Word: Copy, T: Deref<Target = RefCell<U>>, U: Write<Word>> Write<Word> for Shared<T> {
+    fn write(&mut self, word: Word) -> nb::Result<(), Self::Error> {
+        self.0
+            .deref()
+            .try_borrow_mut()
+            .map_err(|_| SharedRefCellError::Contention)?
+            .write(word)
+            .map_err(|err| err.map(SharedRefCellError::Other))
+    }
+
+    fn flush(&mut self) -> nb::Result<(), Self::Error> {
+        self.0
+            .deref()
+            .try_borrow_mut()
+            .map_err(|_| SharedRefCellError::Contention)?
+            .flush()
+            .map_err(|err| err.map(SharedRefCellError::Other))
+    }
 }
