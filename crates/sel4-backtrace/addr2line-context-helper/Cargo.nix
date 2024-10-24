e5a2e880565cdf0c02ae894d64fe0b67621bd3c3
Nick Spinale <nick@nickspinale.com>
2024-10-24 16:45:06 -0700

    no obj feat

--- crates/sel4-backtrace/addr2line-context-helper/Cargo.nix
+++ crates/sel4-backtrace/addr2line-context-helper/Cargo.nix
@@ -9,7 +9,7 @@
 mk {
   package.name = "sel4-backtrace-addr2line-context-helper";
   dependencies = {
-    addr2line = localCrates.addr2line // { version = versions.addr2line; default-features = false; features = [ "rustc-demangle" "cpp_demangle" "fallible-iterator" "smallvec" "object" ]; };
+    addr2line = localCrates.addr2line // { version = versions.addr2line; default-features = false; features = [ "rustc-demangle" "cpp_demangle" "fallible-iterator" "smallvec" ]; };
     gimli = { version = versions.gimli; default-features = false; features = [ "endian-reader" ]; };
     object = { version = versions.object; default-features = false; features = [ "read" ]; };
     stable_deref_trait = { version = "1.1.0"; default-features = false; features = [ "alloc" ]; };
