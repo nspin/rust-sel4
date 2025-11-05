ca83eb1d8ff5f3cf963889ee8c7431a4c44b5d87
Nick Spinale <nick@nickspinale.com>
2025-11-05 01:42:40 -0800

    capdl: organize main

--- crates/sel4-capdl-initializer/src/main.rs
+++ crates/sel4-capdl-initializer/src/main.rs
@@ -26,40 +26,10 @@ use sel4_immutable_cell::ImmutableCell;
 use sel4_logging::{LevelFilter, Logger, LoggerBuilder};
 use sel4_root_task::{debug_print, root_task};
 
-#[cfg(not(feature = "alloc"))]
-mod no_allocator;
-
 // TODO consolidate core into this crate
 
-const LOG_LEVEL: LevelFilter = {
-    // LevelFilter::Trace
-    // LevelFilter::Debug
-    LevelFilter::Info
-};
-
-static LOGGER: Logger = LoggerBuilder::const_default()
-    .level_filter(LOG_LEVEL)
-    .filter(|meta| meta.target() == "sel4_capdl_initializer_core")
-    .write(|s| debug_print!("{}", s))
-    .build();
-
-#[cfg_attr(
-    feature = "alloc",
-    root_task(stack_size = 0x10_000, heap_size = 0x10_000)
-)]
-#[cfg_attr(not(feature = "alloc"), root_task(stack_size = 0x10_000))]
-fn main(bootinfo: &sel4::BootInfoPtr) -> ! {
-    LOGGER.set().unwrap();
-    let spec = access_spec(get_spec_bytes());
-    let mut buffers = InitializerBuffers::new(get_buffers(spec.objects.len()));
-    Initializer::initialize(
-        bootinfo,
-        user_image_bounds(),
-        spec,
-        *sel4_capdl_initializer_embedded_frames_data_start.get() as usize,
-        &mut buffers,
-    )
-}
+#[cfg(not(feature = "alloc"))]
+mod no_allocator;
 
 #[allow(non_upper_case_globals)]
 #[unsafe(no_mangle)]
@@ -91,33 +61,34 @@ static sel4_capdl_initializer_image_start: ImmutableCell<*mut u8> =
 static sel4_capdl_initializer_image_end: ImmutableCell<*mut u8> =
     ImmutableCell::new(ptr::null_mut());
 
-#[cfg(not(feature = "alloc"))]
-fn get_buffers(num_objects: usize) -> impl BorrowMut<[PerObjectBuffer]> {
-    use sel4_one_ref_cell::OneRefCell;
+const LOG_LEVEL: LevelFilter = {
+    // LevelFilter::Trace
+    // LevelFilter::Debug
+    LevelFilter::Info
+};
 
-    const MAX_OBJECTS: usize = 4096;
+static LOGGER: Logger = LoggerBuilder::const_default()
+    .level_filter(LOG_LEVEL)
+    .filter(|meta| meta.target() == "sel4_capdl_initializer_core")
+    .write(|s| debug_print!("{}", s))
+    .build();
 
-    static BUFFERS: OneRefCell<[PerObjectBuffer; MAX_OBJECTS]> =
-        OneRefCell::new([PerObjectBuffer::const_default(); _]);
-
-    assert!(num_objects <= MAX_OBJECTS);
-
-    &mut BUFFERS.take().unwrap().as_mut_slice()[..num_objects]
-}
-
-#[cfg(feature = "alloc")]
-fn get_buffers(num_objects: usize) -> impl BorrowMut<[PerObjectBuffer]> {
-    vec![PerObjectBuffer::const_default(); num_objects]
-}
-
-#[cfg(feature = "alloc")]
-fn access_spec(bytes: &[u8]) -> &<SpecForInitializer as Archive>::Archived {
-    SpecForInitializer::access(bytes).unwrap()
-}
-
-#[cfg(not(feature = "alloc"))]
-fn access_spec(bytes: &[u8]) -> &<SpecForInitializer as Archive>::Archived {
-    unsafe { SpecForInitializer::access_unchecked(bytes) }
+#[cfg_attr(
+    feature = "alloc",
+    root_task(stack_size = 0x10_000, heap_size = 0x10_000)
+)]
+#[cfg_attr(not(feature = "alloc"), root_task(stack_size = 0x10_000))]
+fn main(bootinfo: &sel4::BootInfoPtr) -> ! {
+    LOGGER.set().unwrap();
+    let spec = access_spec(get_spec_bytes());
+    let mut buffers = InitializerBuffers::new(get_buffers(spec.objects.len()));
+    Initializer::initialize(
+        bootinfo,
+        user_image_bounds(),
+        spec,
+        *sel4_capdl_initializer_embedded_frames_data_start.get() as usize,
+        &mut buffers,
+    )
 }
 
 fn get_spec_bytes() -> &'static [u8] {
@@ -132,4 +103,34 @@ fn get_spec_bytes() -> &'static [u8] {
 fn user_image_bounds() -> Range<usize> {
     (*sel4_capdl_initializer_image_start.get() as usize)
         ..(*sel4_capdl_initializer_image_end.get() as usize)
+}
+
+#[cfg(feature = "alloc")]
+fn get_buffers(num_objects: usize) -> impl BorrowMut<[PerObjectBuffer]> {
+    vec![PerObjectBuffer::const_default(); num_objects]
+}
+
+#[cfg(not(feature = "alloc"))]
+fn get_buffers(num_objects: usize) -> impl BorrowMut<[PerObjectBuffer]> {
+    use sel4_one_ref_cell::OneRefCell;
+
+    // TODO make configurable
+    const MAX_OBJECTS: usize = 4096;
+
+    static BUFFERS: OneRefCell<[PerObjectBuffer; MAX_OBJECTS]> =
+        OneRefCell::new([PerObjectBuffer::const_default(); _]);
+
+    assert!(num_objects <= MAX_OBJECTS);
+
+    &mut BUFFERS.take().unwrap().as_mut_slice()[..num_objects]
+}
+
+#[cfg(feature = "alloc")]
+fn access_spec(bytes: &[u8]) -> &<SpecForInitializer as Archive>::Archived {
+    SpecForInitializer::access(bytes).unwrap()
+}
+
+#[cfg(not(feature = "alloc"))]
+fn access_spec(bytes: &[u8]) -> &<SpecForInitializer as Archive>::Archived {
+    unsafe { SpecForInitializer::access_unchecked(bytes) }
 }
