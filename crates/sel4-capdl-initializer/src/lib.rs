6918a96f8bcbf7b4f655c0986089905f906701db
Nick Spinale <nick@nickspinale.com>
2025-11-12 03:07:13 -0800

    [ILLUSTRATIVE] split initializer into [lib] and [main]
    Backported from rust-sel4/HEAD to demonstrate on microkit/HEAD

---
+++ crates/sel4-capdl-initializer/src/lib.rs
@@ -0,0 +1,144 @@
+//
+// Copyright 2023, Colias Group, LLC
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+
+#![no_std]
+
+extern crate alloc;
+
+use alloc::vec;
+use core::ops::Range;
+use core::ptr;
+use core::slice;
+use sel4::{UntypedDesc, sel4_cfg_usize};
+
+use one_shot_mutex::sync::RawOneShotMutex;
+
+use sel4_capdl_initializer_core::{Initializer, InitializerBuffers, PerObjectBuffer};
+use sel4_capdl_initializer_types::{
+    IndirectDeflatedBytesContent, IndirectEmbeddedFrame, IndirectObjectName, SpecWithIndirection,
+    SpecWithSources,
+};
+use sel4_dlmalloc::{DeferredStaticDlmalloc, StaticHeapBounds};
+use sel4_logging::{LevelFilter, Logger, LoggerBuilder};
+use sel4_root_task::{debug_print, root_task};
+
+const LOG_LEVEL: LevelFilter = {
+    // LevelFilter::Trace
+    // LevelFilter::Debug
+    LevelFilter::Info
+};
+
+static LOGGER: Logger = LoggerBuilder::const_default()
+    .level_filter(LOG_LEVEL)
+    .filter(|meta| meta.target() == "sel4_capdl_initializer_core")
+    .write(|s| debug_print!("{}", s))
+    .build();
+
+#[global_allocator]
+static GLOBAL_ALLOCATOR: DeferredStaticDlmalloc<RawOneShotMutex> = DeferredStaticDlmalloc::new();
+
+const UNTYPED_DESC_SIZE: usize = size_of::<UntypedDesc>();
+const MAX_UNTYPEDS: usize = sel4_cfg_usize!(MAX_NUM_BOOTINFO_UNTYPED_CAPS);
+
+#[root_task(stack_size = 0x10000)]
+fn main(bootinfo: &sel4::BootInfoPtr) -> ! {
+    let _ = GLOBAL_ALLOCATOR.set_bounds(static_heap_bounds());
+    LOGGER.set().unwrap();
+    let spec_with_sources = get_spec_with_sources();
+    let mut buffers = InitializerBuffers::new(vec![
+        PerObjectBuffer::const_default();
+        spec_with_sources.spec.objects.len()
+    ]);
+    Initializer::initialize(
+        bootinfo,
+        user_image_bounds(),
+        &spec_with_sources,
+        &mut buffers,
+        get_expected_untypeds(),
+    )
+}
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".data")]
+static mut sel4_capdl_initializer_serialized_spec_start: *mut u8 = ptr::null_mut();
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".data")]
+static mut sel4_capdl_initializer_serialized_spec_size: usize = 0;
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".data")]
+static mut sel4_capdl_initializer_heap_start: *mut u8 = ptr::null_mut();
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".data")]
+static mut sel4_capdl_initializer_heap_size: usize = 0;
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".data")]
+static mut sel4_capdl_initializer_image_start: *mut u8 = ptr::null_mut();
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".data")]
+static mut sel4_capdl_initializer_image_end: *mut u8 = ptr::null_mut();
+
+/// Optional
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".data")]
+static mut sel4_capdl_initializer_expected_untypeds_list: [u8; UNTYPED_DESC_SIZE * MAX_UNTYPEDS] =
+    [0; UNTYPED_DESC_SIZE * MAX_UNTYPEDS];
+
+/// Optional
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".data")]
+static mut sel4_capdl_initializer_expected_untypeds_list_num_entries: usize = 0;
+
+fn get_spec_with_sources<'a>() -> SpecWithSources<
+    'a,
+    Option<IndirectObjectName>,
+    IndirectDeflatedBytesContent,
+    IndirectEmbeddedFrame,
+> {
+    let blob = unsafe {
+        slice::from_raw_parts(
+            sel4_capdl_initializer_serialized_spec_start,
+            sel4_capdl_initializer_serialized_spec_size,
+        )
+    };
+    let (spec, source) = postcard::take_from_bytes::<SpecWithIndirection>(blob).unwrap();
+    SpecWithSources {
+        spec,
+        object_name_source: source,
+        content_source: source,
+        embedded_frame_source: source,
+    }
+}
+
+fn user_image_bounds() -> Range<usize> {
+    unsafe {
+        (sel4_capdl_initializer_image_start as usize)..(sel4_capdl_initializer_image_end as usize)
+    }
+}
+
+fn static_heap_bounds() -> StaticHeapBounds {
+    unsafe {
+        StaticHeapBounds::new(
+            sel4_capdl_initializer_heap_start,
+            sel4_capdl_initializer_heap_size,
+        )
+    }
+}
+
+/// This is useful for error checking when your upstream spec generation tool expects a certain range of untypeds from the kernel.
+fn get_expected_untypeds() -> &'static [UntypedDesc] {
+    #[allow(static_mut_refs)]
+    unsafe {
+        let num_entries = sel4_capdl_initializer_expected_untypeds_list_num_entries
+            .min(sel4::sel4_cfg_usize!(MAX_NUM_BOOTINFO_UNTYPED_CAPS));
+        let ptr = sel4_capdl_initializer_expected_untypeds_list.as_ptr() as *const UntypedDesc;
+        core::slice::from_raw_parts(ptr, num_entries)
+    }
+}
