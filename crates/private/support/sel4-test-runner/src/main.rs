568b495ad03027d027bd9cef903c948fe3ad1a44
Nick Spinale <nick@nickspinale.com>
2026-04-08 16:43:09 -0700

    x

--- crates/private/support/sel4-test-runner/src/main.rs
+++ crates/private/support/sel4-test-runner/src/main.rs
@@ -14,8 +14,19 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(long)]
-    target_dir: String,
-
+    target_dir: PathBuf,
+    #[arg(long)]
+    object_sizes: Option<PathBuf>,
+    #[arg(long)]
+    sel4_kernel_config: Option<PathBuf>,
+    #[arg(long)]
+    simulate_script: PathBuf,
+    #[arg(long)]
+    microkit_tool: Option<PathBuf>,
+    #[arg(long)]
+    microkit_board: String,
+    #[arg(long)]
+    microkit_config: String,
     #[arg(short, long, default_value_t = 1)]
     count: u8,
 }
@@ -23,4 +34,3 @@ struct Args {
 fn main() -> ExitCode {
     ExitCode::SUCCESS
 }
-
