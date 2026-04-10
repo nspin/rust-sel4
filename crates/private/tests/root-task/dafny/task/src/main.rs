ee42c393e846fe3870d98d6990ef3a643ba78730
Nick Spinale <nick@nickspinale.com>
2026-04-10 03:14:17 -0700

    x

--- crates/private/tests/root-task/dafny/task/src/main.rs
+++ crates/private/tests/root-task/dafny/task/src/main.rs
@@ -12,7 +12,5 @@ use sel4_root_task::{debug_println, root_task};
 #[root_task(heap_size = 1024 * 1024)]
 fn main(_: &sel4::BootInfoPtr) -> ! {
     assert_eq!(tests_root_task_dafny_core::max(13, 37), 37);
-
-    debug_println!("TEST_PASS");
-    sel4::init_thread::suspend_self()
+    sel4_test_root_task::indicate_success()
 }
