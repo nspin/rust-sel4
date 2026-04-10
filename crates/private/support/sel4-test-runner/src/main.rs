5468a368c2d7b1a1cfa5e0842403438e466ea46a
Nick Spinale <nick@nickspinale.com>
2026-04-10 00:50:06 -0700

    x

--- crates/private/support/sel4-test-runner/src/main.rs
+++ crates/private/support/sel4-test-runner/src/main.rs
@@ -303,8 +303,7 @@ impl<'a> Runner<'a> {
                 .arg(self.get_capdl_initializer_target_config())
                 .arg("--target-dir")
                 .arg(&self.cli.target_dir)
-                .arg("-p")
-                .arg("sel4-capdl-initializer")
+                .arg("-p=sel4-capdl-initializer")
                 .arg("--artifact-dir")
                 .arg(self.d)
                 .status()?
