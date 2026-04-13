2f231a717b320fdba72437386b3bc9f169b62a26
Nick Spinale <nick@nickspinale.com>
2026-04-13 02:23:34 -0700

    x

--- crates/private/support/sel4-test-sentinels/wrapper/src/main.rs
+++ crates/private/support/sel4-test-sentinels/wrapper/src/main.rs
@@ -18,5 +18,7 @@ fn main() -> Result<(), Error> {
     cmd.args(child_args);
     sel4_test_sentinels_wrapper::default_sentinels()
         .wrap(cmd)?
-        .success_ok()
+        .success_ok()?;
+    println!();
+    Ok(())
 }
