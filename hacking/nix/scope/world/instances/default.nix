13ae3eb35ac62cc48a3982ad62a71a0c40909968
Nick Spinale <nick@nickspinale.com>
2026-04-13 02:57:36 -0700

    x

--- hacking/nix/scope/world/instances/default.nix
+++ hacking/nix/scope/world/instances/default.nix
@@ -106,7 +106,7 @@ in rec {
     # examples.root-task.hello
     examples.root-task.example-root-task
     examples.root-task.example-root-task-without-runtime
-    # examples.root-task.spawn-thread
+    examples.root-task.spawn-thread
     # examples.root-task.spawn-task
     # examples.root-task.serial-device
 
