d0b0a3b47a0c933c914d2118708dfdc6e7730885
Nick Spinale <nick@nickspinale.com>
2026-04-13 01:35:08 -0700

    x

--- crates/private/support/sel4-test-sentinels/wrapper/src/main.rs
+++ crates/private/support/sel4-test-sentinels/wrapper/src/main.rs
@@ -9,6 +9,8 @@ use std::process::Command;
 
 use anyhow::Error;
 
+use sel4_test_sentinels_wrapper::*;
+
 fn main() -> Result<(), Error> {
     let mut args = env::args_os();
     let _program_name = args.next();
@@ -16,5 +18,5 @@ fn main() -> Result<(), Error> {
     let child_args = args.collect::<Vec<_>>();
     let mut cmd = Command::new(child_program);
     cmd.args(child_args);
-    sel4_test_sentinels_wrapper::run(cmd)?.success_ok()
+    default_sentinels().wrap(cmd)?.map(|v| *v).success_ok()
 }
