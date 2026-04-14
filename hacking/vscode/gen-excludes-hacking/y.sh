e4da3d10d999f8be9be21f098cfe4e39811c8994
Nick Spinale <nick@nickspinale.com>
2026-04-14 01:10:00 -0700

    WIP: vscode

---
+++ hacking/vscode/gen-excludes-hacking/y.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+here="$(dirname $0)"
+
+# comm -23 \
+#   <(cargo metadata --no-deps --format-version 1 | jq -r '.packages[].name' | sort -u) \
+#   <(cargo tree --workspace \
+#     --invert sel4-config-data \
+#     --invert sel4-test-harness \
+#     --invert sel4-reset \
+#     --invert tests-root-task-dafny-core \
+#     --prefix none --format '{p}' | cut -d ' ' -f 1 | sort -u) \
+#   > $here/excludes.txt
+
+cargo tree --workspace \
+    --invert sel4-config-data \
+    --invert sel4-test-harness \
+    --invert sel4-reset \
+    --invert tests-root-task-dafny-core \
+    --prefix none --format '{p}' \
+| cut -d ' ' -f 1 \
+| sort -u \
+> $here/../excludes.txt
+
+
+# cargo tree --workspace \
+#     --invert sel4-config-data \
+#     --depth 1 \
+#     --prefix none --format '{p}'
