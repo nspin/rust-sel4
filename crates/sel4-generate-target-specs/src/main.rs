e50ad0125cc6c3b72efccc83440ec930934153a9
Nick Spinale <nick@nickspinale.com>
2026-04-13 23:46:29 -0700

    x

--- crates/sel4-generate-target-specs/src/main.rs
+++ crates/sel4-generate-target-specs/src/main.rs
@@ -181,6 +181,9 @@ impl Config {
     }
 
     fn filter(&self) -> bool {
+        if self.resettable && self.context != Context::Microkit {
+            return false;
+        }
         if self.unwind && !self.arch.unwinding_support() {
             return false;
         }
