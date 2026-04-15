f54b63ad9efaa2f61043de5275f0b6cc07e6b08e
Nick Spinale <nick@nickspinale.com>
2026-04-15 02:20:20 -0700

    Remove resettable targets

--- hacking/nix/scope/world/instances/microkit/default.nix
+++ hacking/nix/scope/world/instances/microkit/default.nix
@@ -155,7 +155,6 @@ in {
             rootCrate = crates.tests-microkit-reset;
             targetTriple = mkSeL4RustTargetTriple {
               microkit = true;
-              resettable = true;
               minimal = false;
             };
             release = false;
