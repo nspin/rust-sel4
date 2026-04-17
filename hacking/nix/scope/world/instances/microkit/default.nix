40854baecb3982ca094d6fce4a021284ca96c5d7
Nick Spinale <nick@nickspinale.com>
2026-04-16 22:07:01 -0700

    Remove unnecessary supplementary object files
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- hacking/nix/scope/world/instances/microkit/default.nix
+++ hacking/nix/scope/world/instances/microkit/default.nix
@@ -163,10 +163,6 @@ in {
           origELF = "${orig}/bin/test.elf";
 
           patched = prepareResettable origELF;
-
-          sup = runCommandCC "test.sup.elf" {} ''
-            $OBJCOPY --only-keep-debug ${origELF} $out
-          '';
         };
       in
         callPlatform {
@@ -174,9 +170,6 @@ in {
             searchPath = [
               (linkFarm "pd" {
                 "test.elf" = pd.patched;
-              })
-              (linkFarm "pd" {
-                "test.sup.elf" = pd.sup;
               })
             ];
             systemXML = sources.srcRoot + "/crates/private/tests/microkit/reset/src/bin/system.xml";
@@ -187,7 +180,6 @@ in {
           extraDebuggingLinks = [
             { name = "test.orig.elf"; path = pd.origELF; }
             { name = "test.patched.elf"; path = pd.patched; }
-            { name = "test.sup.elf"; path = pd.sup; }
           ];
         } // {
           inherit pd;
