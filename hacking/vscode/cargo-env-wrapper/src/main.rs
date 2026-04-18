7db21ff18b7813166207e0629ea31ed2cdb4b017
Nick Spinale <nick@nickspinale.com>
2026-04-18 02:51:39 -0700

    x

--- hacking/vscode/cargo-env-wrapper/src/main.rs
+++ hacking/vscode/cargo-env-wrapper/src/main.rs
@@ -10,62 +10,51 @@ use std::path::PathBuf;
 
 use cargo_metadata::MetadataCommand;
 use clap::Parser;
-use similar::TextDiff;
 
 #[derive(Debug, Parser)]
 struct Cli {
     #[arg(long)]
-    manifest_path: PathBuf,
+    manifest_path: Option<PathBuf>,
 
-    #[arg(short = 'o')]
-    out: PathBuf,
+    #[arg(long, short = 'F')]
+    features: Vec<String>,
 
-    #[arg(long = "just-check")]
-    just_check: bool,
+    #[arg(long)]
+    all_features: bool,
+
+    #[arg(long)]
+    no_default_features: bool,
+
+    #[arg(long)]
+    target: Option<String>,
+
+    #[arg(long)]
+    config: Vec<String>,
 }
 
 fn main() {
-    let args = Cli::parse();
-
-    let metadata = MetadataCommand::new()
-        .manifest_path(&args.manifest_path)
-        .no_deps()
-        .exec()
-        .unwrap();
-
-    let default_members = metadata
-        .workspace_default_members
-        .iter()
-        .collect::<BTreeSet<_>>();
-
-    let mut excludes = BTreeSet::new();
-    for pkg in metadata.workspace_packages() {
-        if !default_members.contains(&pkg.id) {
-            excludes.insert(pkg.name.as_str());
-        }
-    }
-    let excludes_str = excludes_to_string(&excludes);
-
-    if args.just_check {
-        let existing_excludes_input_str = fs::read_to_string(&args.out).unwrap();
-        let existing_excludes = existing_excludes_input_str.lines().collect::<BTreeSet<_>>();
-        if excludes != existing_excludes {
-            let existing_excludes_str = excludes_to_string(&existing_excludes);
-            panic!(
-                "mismatch:\n{}",
-                TextDiff::from_lines(&excludes_str, &existing_excludes_str).unified_diff(),
-            );
-        }
-    } else {
-        fs::write(args.out, excludes_str).unwrap();
-    }
+    let cli = Cli::parse();
 }
 
-fn excludes_to_string(excludes: &BTreeSet<&str>) -> String {
-    let mut s = String::new();
-    for pkg_name in excludes {
-        s.push_str(pkg_name);
-        s.push('\n');
+fn forward_args(cli: &Cli) -> Vec<String> {
+    let mut args = vec![];
+    for s in cli.features.iter() {
+        args.push("--features".to_owned());
+        args.push(s.to_owned());
     }
-    s
+    if cli.all_features {
+        args.push("--all-features".to_owned());
+    }
+    if cli.no_default_features {
+        args.push("--all-features".to_owned());
+    }
+    if let Some(s) = cli.target.as_ref() {
+        args.push("--target".to_owned());
+        args.push(s.to_owned());
+    }
+    for s in cli.config.iter() {
+        args.push("--config".to_owned());
+        args.push(s.to_owned());
+    }
+    args
 }
