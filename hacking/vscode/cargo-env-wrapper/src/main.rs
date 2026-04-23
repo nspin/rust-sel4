7b1ca38f819ab7a5dfb703f27e9cf1e348588d52
Nick Spinale <nick@nickspinale.com>
2026-04-23 15:40:29 -0700

    x

--- hacking/vscode/cargo-env-wrapper/src/main.rs
+++ hacking/vscode/cargo-env-wrapper/src/main.rs
@@ -11,6 +11,7 @@ use std::process::Command;
 
 use cargo_metadata::{MetadataCommand, PackageName};
 use clap::Parser;
+use serde_json::{Value, json};
 
 fn main() {
     Env::get().run()
@@ -75,26 +76,76 @@ impl Env {
         } else {
             BTreeSet::new()
         };
+        let mut ws_args = self.forward_features_args();
+        for pkg in excludes.iter() {
+            ws_args.push("--exclude".to_owned());
+            ws_args.push(pkg.to_string());
+        }
+        //     let ws = json!({
+        // // "rust-analyzer.cargo.extraArgs": [
+        // //     "--config", "/home/x/i/rust-sel4/.cargo/config.toml",
+        // // 	"--config", "/home/x/i/rust-sel4/.cargo/gen/target/aarch64-sel4-microkit.toml",
+        // // 	"--config", "/home/x/i/rust-sel4/.cargo/gen/world/aarch64.microkitDefault.toml",
+        // // ],
+
+        //     });
         if self.cli.just_dump_excludes {
             for pkg in excludes.iter() {
                 println!("{pkg}");
             }
         } else {
-            let mut cmd = Command::new("/home/x/i/rust-sel4/hacking/vscode/cargo-env-defaults-wrapper");
+            let mut cmd =
+                Command::new("/home/x/i/rust-sel4/hacking/vscode/cargo-env-defaults-wrapper");
             // let mut cmd = Command::new(&self.cli.wrapped);
             cmd.arg(&self.cli.wrapped);
             cmd.args(&self.cli.wrapped_args);
-            cmd.env("__RUST_ANALYZER_WRAPPER__ARGS", self.forward_config_args().join(" "));
-            let mut ws_args = self.forward_features_args();
-            for pkg in excludes.iter() {
-                ws_args.push("--exclude".to_owned());
-                ws_args.push(pkg.to_string());
-            }
+            cmd.env(
+                "__RUST_ANALYZER_WRAPPER__ARGS",
+                self.forward_config_args().join(" "),
+            );
             cmd.env("__RUST_ANALYZER_WRAPPER__WORKSPACE_ARGS", ws_args.join(" "));
             // assert!(cmd.spawn().unwrap().wait().unwrap().success());
             println!("{:?}", cmd);
             panic!("{:?}", cmd.exec());
         }
+    }
+
+    fn ws(&self, excludes: BTreeSet<PackageName>) -> Value {
+        let mut ws = json!({
+            "rust-analyzer.cargo.extraArgs": excludes.iter().map(|x| x.to_string()).collect::<Vec<_>>(),
+        });
+        let mut features: Option<Vec<&str>> = None;
+        for s in self.cli.features.iter() {
+            features.get_or_insert_default().push(s);
+        }
+        let features_val = if self.cli.all_features {
+            if features.is_some() {
+                panic!()
+            }
+            Some(Value::String("all".to_owned()))
+        } else if let Some(features) = features {
+            Some(Value::Array(
+                features
+                    .into_iter()
+                    .map(|s| Value::String(s.to_owned()))
+                    .collect::<Vec<_>>(),
+            ))
+        } else {
+            None
+        };
+        if let Some(features_val) = features_val {
+            ws.as_object_mut()
+                .unwrap()
+                .insert("rust-analyzer.cargo.features".to_owned(), features_val);
+        }
+        if self.cli.no_default_features {
+            ws.as_object_mut().unwrap().insert(
+                "rust-analyzer.cargo.noDefaultFeatures".to_owned(),
+                Value::Bool(true),
+            );
+        }
+
+        ws
     }
 
     fn via_includes(&self) -> BTreeSet<PackageName> {
@@ -153,7 +204,7 @@ impl Env {
                 })
                 .collect::<BTreeSet<_>>()
         };
-    
+
         let all = all_pre
             .iter()
             .map(|name| pkg_names[name.as_str()])
@@ -347,9 +398,7 @@ impl Env {
         args
     }
 
-    fn forward_config_args(
-        &self,
-    ) -> Vec<String> {
+    fn forward_config_args(&self) -> Vec<String> {
         let mut args = vec![];
         if let Some(s) = self.cli.target.as_ref() {
             args.push("--config".to_owned());
