aed8a1b99cfe57b8b84c05badbe7ba3b0c83191a
Nick Spinale <nick@nickspinale.com>
2026-05-10 04:16:19 -0700

    x

--- crates/sel4-kernel-loader/add-payload/src/maps.rs
+++ crates/sel4-kernel-loader/add-payload/src/maps.rs
@@ -9,7 +9,12 @@
 
 use std::ops::Range;
 
+use quote::format_ident;
+
 use crate::platform_info::PlatformInfoForBuildSystem;
+use crate::page_tables::{
+    LeafLocation, Region, RegionsBuilder, Scheme, SchemeHelpers, schemes,
+};
 
 pub const ALIGN: u64 = 4096;
 
@@ -22,5 +27,141 @@ pub fn mk_kernel_map(
     kernel_phys_addr_range: Range<u64>,
     kernel_phys_to_virt_offset: u64,
 ) -> Vec<u8> {
+    let virt_start = kernel_phys_addr_range
+        .start
+        .wrapping_add(kernel_phys_to_virt_offset);
+    let virt_end = kernel_phys_addr_range
+        .end
+        .wrapping_add(kernel_phys_to_virt_offset);
+    let virt_map_end =
+        virt_end.next_multiple_of(1 << SchemeHelpers::<SchemeImpl>::largest_leaf_size_bits());
+
+    let regions = RegionsBuilder::<SchemeImpl>::new()
+        .insert(Region::valid(
+            0..virt_start,
+            SchemeImpl::mk_identity_leaf_for_kernel_map,
+        ))
+        .insert(Region::valid(virt_start..virt_map_end, move |loc| {
+            SchemeImpl::mk_kernel_leaf_for_kernel_map(kernel_phys_to_virt_offset, loc)
+        }));
+
+    regions.build().construct_table().embed(
+        format_ident!("kernel_boot_level_0_table"),
+        format_ident!("sel4_kernel_loader_embed_page_tables_runtime"),
+    );
     vec![]
+}
+
+type SchemeImpl = schemes::RiscV32Sv32;
+
+trait SchemeExt: Scheme {
+    fn mk_normal_leaf_for_loader_map(_loc: LeafLocation) -> Self::LeafDescriptor {
+        unimplemented!()
+    }
+
+    fn mk_device_leaf_for_loader_map(_loc: LeafLocation) -> Self::LeafDescriptor {
+        unimplemented!()
+    }
+
+    fn mk_identity_leaf_for_kernel_map(loc: LeafLocation) -> Self::LeafDescriptor;
+
+    fn mk_kernel_leaf_for_kernel_map(
+        phys_to_virt_offset: u64,
+        loc: LeafLocation,
+    ) -> Self::LeafDescriptor;
+}
+
+impl SchemeExt for schemes::AArch64 {
+    fn mk_normal_leaf_for_loader_map(loc: LeafLocation) -> Self::LeafDescriptor {
+        loc.map_identity::<schemes::AArch64>()
+            .set_access_flag(true)
+            .set_attribute_index(4) // select MT_NORMAL
+            .set_shareability(AARCH64_NORMAL_SHAREABILITY)
+    }
+
+    fn mk_device_leaf_for_loader_map(loc: LeafLocation) -> Self::LeafDescriptor {
+        loc.map_identity::<schemes::AArch64>()
+            .set_access_flag(true)
+            .set_attribute_index(0) // select MT_DEVICE_nGnRnE
+    }
+
+    fn mk_identity_leaf_for_kernel_map(loc: LeafLocation) -> Self::LeafDescriptor {
+        loc.map_identity::<schemes::AArch64>()
+            .set_access_flag(true)
+            .set_attribute_index(0) // select MT_DEVICE_nGnRnE
+    }
+
+    fn mk_kernel_leaf_for_kernel_map(
+        phys_to_virt_offset: u64,
+        loc: LeafLocation,
+    ) -> Self::LeafDescriptor {
+        loc.map::<schemes::AArch64>(|vaddr| virt_to_phys(vaddr, phys_to_virt_offset))
+            .set_access_flag(true)
+            .set_attribute_index(4) // select MT_NORMAL
+            .set_shareability(AARCH64_NORMAL_SHAREABILITY)
+    }
+}
+
+// TODO
+const AARCH64_NORMAL_SHAREABILITY: u64 = 0;
+// if todo!("MAX_NUM_NODES") {
+//     0b11
+// } else {
+//     0b00
+// };
+
+impl SchemeExt for schemes::AArch32 {
+    fn mk_normal_leaf_for_loader_map(loc: LeafLocation) -> Self::LeafDescriptor {
+        loc.map_identity::<schemes::AArch32>()
+            .set_access_flag(true)
+            .set_attributes(0b101, false, true)
+            .set_shareability(true)
+    }
+
+    fn mk_device_leaf_for_loader_map(loc: LeafLocation) -> Self::LeafDescriptor {
+        loc.map_identity::<schemes::AArch32>().set_access_flag(true)
+    }
+
+    fn mk_identity_leaf_for_kernel_map(loc: LeafLocation) -> Self::LeafDescriptor {
+        loc.map_identity::<schemes::AArch32>().set_access_flag(true)
+    }
+
+    fn mk_kernel_leaf_for_kernel_map(
+        phys_to_virt_offset: u64,
+        loc: LeafLocation,
+    ) -> Self::LeafDescriptor {
+        loc.map::<schemes::AArch32>(|vaddr| virt_to_phys(vaddr, phys_to_virt_offset))
+            .set_access_flag(true)
+            .set_shareability(true)
+    }
+}
+
+impl SchemeExt for schemes::RiscV64Sv39 {
+    fn mk_identity_leaf_for_kernel_map(loc: LeafLocation) -> Self::LeafDescriptor {
+        loc.map_identity::<Self>()
+    }
+
+    fn mk_kernel_leaf_for_kernel_map(
+        phys_to_virt_offset: u64,
+        loc: LeafLocation,
+    ) -> Self::LeafDescriptor {
+        loc.map::<Self>(|vaddr| virt_to_phys(vaddr, phys_to_virt_offset))
+    }
+}
+
+impl SchemeExt for schemes::RiscV32Sv32 {
+    fn mk_identity_leaf_for_kernel_map(loc: LeafLocation) -> Self::LeafDescriptor {
+        loc.map_identity::<Self>()
+    }
+
+    fn mk_kernel_leaf_for_kernel_map(
+        phys_to_virt_offset: u64,
+        loc: LeafLocation,
+    ) -> Self::LeafDescriptor {
+        loc.map::<Self>(|vaddr| virt_to_phys(vaddr, phys_to_virt_offset))
+    }
+}
+
+fn virt_to_phys(vaddr: u64, phys_to_virt_offset: u64) -> u64 {
+    vaddr.wrapping_sub(phys_to_virt_offset)
 }
