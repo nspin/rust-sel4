7fec8eaa68ddd65135a532834c3ecdcc8805da0d
Nick Spinale <nick@nickspinale.com>
2026-05-12 15:34:58 -0700

    x

--- crates/sel4-kernel-loader/add-payload/src/page_tables2/scheme.rs
+++ crates/sel4-kernel-loader/add-payload/src/page_tables2/scheme.rs
@@ -11,26 +11,33 @@ use std::ops::Range;
 
 use bitfield::{BitMut, BitRange, BitRangeMut};
 
+pub type RawEntry = u64;
+
 pub type RawAttributes = u64;
 
 pub trait ArchAttributes {
-    fn from_level_paddr(level: u8, paddr: u64) -> RawAttributes;
+    fn new(level: u8) -> RawAttributes;
+    fn leaf(attrs: &Attributes<Self>, paddr: u64) -> RawEntry;
 }
 
 #[derive(Debug)]
-pub struct Attributes<T> {
+pub struct Attributes<T: ?Sized> {
     _phantom: PhantomData<T>,
     level: u8,
     raw: RawAttributes,
 }
 
 impl<T: ArchAttributes> Attributes<T> {
-    pub fn from_level_paddr(level: u8, paddr: u64) -> Self {
+    pub fn from_level_paddr(level: u8) -> Self {
         Self {
             _phantom: PhantomData,
             level,
-            raw: T::from_level_paddr(level, paddr),
+            raw: T::new(level),
         }
+    }
+
+    pub fn leaf(&self, paddr: u64) -> RawEntry {
+        T::leaf(self, paddr)
     }
 }
 
@@ -38,7 +45,66 @@ impl<T: ArchAttributes> Attributes<T> {
 pub enum AArch64 {}
 
 impl ArchAttributes for AArch64{
-    fn from_level_paddr(level: u8, paddr: u64) -> RawAttributes {
-        desc.set_bit_range(1, 0, if level == 3 { 0b11 } else { 0b01 });
+    fn new(level: u8) -> RawAttributes {
+        let mut attrs = 0;
+        attrs.set_bit_range(1, 0, if level == 3 { 0b11 } else { 0b01 });
+        attrs
+    }
+
+    fn leaf(attrs: &Attributes<Self>, paddr: u64) -> RawEntry {
+        attrs.raw | paddr
+    }
+}
+
+impl Attributes<AArch64> {
+    pub fn set_access_flag(mut self, value: bool) -> Self {
+        self.raw.set_bit(10, value);
+        self
+    }
+
+    pub fn set_attribute_index(mut self, index: u64) -> Self {
+        assert_eq!(index >> 3, 0);
+        self.raw.set_bit_range(4, 2, index);
+        self
+    }
+
+    pub fn set_shareability(mut self, shareability: u64) -> Self {
+        assert_eq!(shareability >> 2, 0);
+        self.raw.set_bit_range(9, 8, shareability);
+        self
+    }
+}
+
+#[derive(Debug)]
+pub enum AArch32 {}
+
+impl ArchAttributes for AArch32{
+    fn new(level: u8) -> RawAttributes {
+        let mut attrs = 0;
+        attrs.set_bit_range(1, 0, 0b10);
+        attrs
+    }
+
+    fn leaf(attrs: &Attributes<Self>, paddr: u64) -> RawEntry {
+        attrs.raw | paddr
+    }
+}
+
+impl Attributes<AArch64> {
+    pub fn set_access_flag(mut self, value: bool) -> Self {
+        self.raw.set_bit(10, value);
+        self
+    }
+
+    pub fn set_attribute_index(mut self, index: u64) -> Self {
+        assert_eq!(index >> 3, 0);
+        self.raw.set_bit_range(4, 2, index);
+        self
+    }
+
+    pub fn set_shareability(mut self, shareability: u64) -> Self {
+        assert_eq!(shareability >> 2, 0);
+        self.raw.set_bit_range(9, 8, shareability);
+        self
     }
 }
