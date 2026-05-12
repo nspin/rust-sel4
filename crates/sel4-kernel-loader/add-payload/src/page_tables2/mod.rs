209dd4b3928fe6b75a6b539119675a45952fe3f9
Nick Spinale <nick@nickspinale.com>
2026-05-12 15:27:46 -0700

    x

--- crates/sel4-kernel-loader/add-payload/src/page_tables2/mod.rs
+++ crates/sel4-kernel-loader/add-payload/src/page_tables2/mod.rs
@@ -4,17 +4,17 @@
 // SPDX-License-Identifier: BSD-2-Clause
 //
 
-mod embed;
-mod glue;
+// mod embed;
+// mod glue;
 mod regions;
 mod scheme;
-mod table;
+// mod table;
 
-pub use glue::{Region, Regions, RegionsBuilder};
-pub use regions::{AbstractRegion, AbstractRegions, AbstractRegionsBuilder};
-pub use scheme::{Scheme, SchemeHelpers};
-pub use table::{LeafLocation, MkLeafFn, RegionContent, Table};
+// pub use glue::{Region, Regions, RegionsBuilder};
+// pub use regions::{AbstractRegion, AbstractRegions, AbstractRegionsBuilder};
+// pub use scheme::{Scheme, SchemeHelpers};
+// pub use table::{LeafLocation, MkLeafFn, RegionContent, Table};
 
-pub mod schemes {
-    pub use super::scheme::{AArch32, AArch64, RiscV32Sv32, RiscV64Sv39};
-}
+// pub mod schemes {
+//     pub use super::scheme::{AArch32, AArch64, RiscV32Sv32, RiscV64Sv39};
+// }
