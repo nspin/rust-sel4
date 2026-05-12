5df4273ea087f1d77b2dcc6a98c44c13bedf6d11
Nick Spinale <nick@nickspinale.com>
2026-05-12 15:46:02 -0700

    x

--- crates/sel4-kernel-loader/add-payload/src/page_tables2/scheme.rs
+++ crates/sel4-kernel-loader/add-payload/src/page_tables2/scheme.rs
@@ -11,100 +11,184 @@ use std::ops::Range;
 
 use bitfield::{BitMut, BitRange, BitRangeMut};
 
-pub type RawEntry = u64;
+pub type RawDescriptor = u64;
 
-pub type RawAttributes = u64;
-
-pub trait ArchAttributes {
-    fn new(level: u8) -> RawAttributes;
-    fn leaf(attrs: &Attributes<Self>, paddr: u64) -> RawEntry;
+pub trait LeafDescriptor {
+    fn from_level_paddr(level: u8, paddr: u64) -> Self;
+    fn to_raw(self) -> RawDescriptor;
 }
 
 #[derive(Debug)]
-pub struct Attributes<T: ?Sized> {
-    _phantom: PhantomData<T>,
+pub struct AArch64LeafDescriptor {
+    raw: RawDescriptor,
+}
+
+impl LeafDescriptor for AArch64LeafDescriptor {
+    fn from_level_paddr(level: u8, paddr: u64) -> Self {
+        let mut raw = paddr;
+        raw.set_bit_range(1, 0, if level == 3 { 0b11 } else { 0b01 });
+        Self { raw }
+    }
+
+    fn to_raw(self) -> RawDescriptor {
+        self.raw
+    }
+}
+
+impl AArch64LeafDescriptor {
+    pub fn set_access_flag(mut self, value: bool) -> Self {
+        self.raw.set_bit(10, value);
+        self
+    }
+
+    pub fn set_attribute_index(mut self, index: u64) -> Self {
+        assert_eq!(index >> 3, 0);
+        self.raw.set_bit_range(4, 2, index);
+        self
+    }
+
+    pub fn set_shareability(mut self, shareability: u64) -> Self {
+        assert_eq!(shareability >> 2, 0);
+        self.raw.set_bit_range(9, 8, shareability);
+        self
+    }
+}
+
+#[derive(Debug)]
+pub struct AArch32LeafDescriptor {
     level: u8,
-    raw: RawAttributes,
+    raw: RawDescriptor,
 }
 
-impl<T: ArchAttributes> Attributes<T> {
-    pub fn from_level_paddr(level: u8) -> Self {
-        Self {
-            _phantom: PhantomData,
-            level,
-            raw: T::new(level),
-        }
+impl LeafDescriptor for AArch32LeafDescriptor {
+    fn from_level_paddr(level: u8, paddr: u64) -> Self {
+        let mut raw = paddr;
+        raw.set_bit_range(1, 0, if level == 3 { 0b11 } else { 0b01 });
+        Self { level, raw }
     }
 
-    pub fn leaf(&self, paddr: u64) -> RawEntry {
-        T::leaf(self, paddr)
+    fn to_raw(self) -> RawDescriptor {
+        self.raw
+    }
+}
+
+impl AArch32LeafDescriptor {
+    pub fn set_access_flag(mut self, value: bool) -> Self {
+        let ix = match self.level {
+            0 => 10,
+            1 => 4,
+            _ => unreachable!(),
+        };
+        self.raw.set_bit(ix, value);
+        self
+    }
+
+    pub fn set_attributes(mut self, tex: u32, c: bool, b: bool) -> Self {
+        assert_eq!(tex >> 3, 0);
+        let (tex_hi, tex_lo) = match self.level {
+            0 => (14, 12),
+            1 => (8, 6),
+            _ => unreachable!(),
+        };
+        self.raw.set_bit_range(tex_hi, tex_lo, tex);
+        self.raw.set_bit(3, c);
+        self.raw.set_bit(2, b);
+        self
+    }
+
+    pub fn set_shareability(mut self, value: bool) -> Self {
+        let ix = match self.level {
+            0 => 16,
+            1 => 10,
+            _ => unreachable!(),
+        };
+        self.raw.set_bit(ix, value);
+        self
     }
 }
 
 #[derive(Debug)]
-pub enum AArch64 {}
+pub struct RiscV64Sv39LeafDescriptor {
+    raw: RawDescriptor,
+}
 
-impl ArchAttributes for AArch64{
-    fn new(level: u8) -> RawAttributes {
-        let mut attrs = 0;
-        attrs.set_bit_range(1, 0, if level == 3 { 0b11 } else { 0b01 });
-        attrs
+impl LeafDescriptor for RiscV64Sv39LeafDescriptor {
+    fn from_level_paddr(_level: u8, paddr: u64) -> Self {
+        let mut raw = 0;
+        raw.set_bit_range(53, 10, BitRange::<u64>::bit_range(&paddr, 55, 12));
+        Self { raw }
+            .set_valid(true)
+            .set_read(true)
+            .set_write(true)
+            .set_execute(true)
     }
 
-    fn leaf(attrs: &Attributes<Self>, paddr: u64) -> RawEntry {
-        attrs.raw | paddr
+    fn to_raw(self) -> RawDescriptor {
+        self.raw
     }
 }
 
-impl Attributes<AArch64> {
-    pub fn set_access_flag(mut self, value: bool) -> Self {
-        self.raw.set_bit(10, value);
+impl RiscV64Sv39LeafDescriptor {
+    pub fn set_valid(mut self, value: bool) -> Self {
+        self.raw.set_bit(0, value);
         self
     }
 
-    pub fn set_attribute_index(mut self, index: u64) -> Self {
-        assert_eq!(index >> 3, 0);
-        self.raw.set_bit_range(4, 2, index);
+    pub fn set_read(mut self, value: bool) -> Self {
+        self.raw.set_bit(1, value);
         self
     }
 
-    pub fn set_shareability(mut self, shareability: u64) -> Self {
-        assert_eq!(shareability >> 2, 0);
-        self.raw.set_bit_range(9, 8, shareability);
+    pub fn set_write(mut self, value: bool) -> Self {
+        self.raw.set_bit(2, value);
+        self
+    }
+
+    pub fn set_execute(mut self, value: bool) -> Self {
+        self.raw.set_bit(3, value);
         self
     }
 }
 
 #[derive(Debug)]
-pub enum AArch32 {}
+pub struct RiscV32Sv32LeafDescriptor {
+    raw: RawDescriptor,
+}
 
-impl ArchAttributes for AArch32{
-    fn new(level: u8) -> RawAttributes {
-        let mut attrs = 0;
-        attrs.set_bit_range(1, 0, 0b10);
-        attrs
+impl LeafDescriptor for RiscV32Sv32LeafDescriptor {
+    fn from_level_paddr(_level: u8, paddr: u64) -> Self {
+        let mut raw = 0;
+        raw.set_bit_range(29, 10, BitRange::<u32>::bit_range(&paddr, 31, 12));
+        Self { raw }
+            .set_valid(true)
+            .set_read(true)
+            .set_write(true)
+            .set_execute(true)
     }
 
-    fn leaf(attrs: &Attributes<Self>, paddr: u64) -> RawEntry {
-        attrs.raw | paddr
+    fn to_raw(self) -> RawDescriptor {
+        self.raw
     }
 }
 
-impl Attributes<AArch64> {
-    pub fn set_access_flag(mut self, value: bool) -> Self {
-        self.raw.set_bit(10, value);
+impl RiscV32Sv32LeafDescriptor {
+    pub fn set_valid(mut self, value: bool) -> Self {
+        self.raw.set_bit(0, value);
         self
     }
 
-    pub fn set_attribute_index(mut self, index: u64) -> Self {
-        assert_eq!(index >> 3, 0);
-        self.raw.set_bit_range(4, 2, index);
+    pub fn set_read(mut self, value: bool) -> Self {
+        self.raw.set_bit(1, value);
         self
     }
 
-    pub fn set_shareability(mut self, shareability: u64) -> Self {
-        assert_eq!(shareability >> 2, 0);
-        self.raw.set_bit_range(9, 8, shareability);
+    pub fn set_write(mut self, value: bool) -> Self {
+        self.raw.set_bit(2, value);
+        self
+    }
+
+    pub fn set_execute(mut self, value: bool) -> Self {
+        self.raw.set_bit(3, value);
         self
     }
 }
