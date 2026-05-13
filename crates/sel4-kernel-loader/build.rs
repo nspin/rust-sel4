f425adcdee8a20802a90360b86544e2e20a469e1
Nick Spinale <nick@nickspinale.com>
2026-05-12 17:51:25 -0700

    x

--- crates/sel4-kernel-loader/build.rs
+++ crates/sel4-kernel-loader/build.rs
@@ -6,39 +6,32 @@
 
 #![allow(clippy::useless_conversion)]
 
-use std::env;
 use std::fs;
 use std::ops::Range;
-use std::path::PathBuf;
 
 use object::{
     ReadRef,
     elf::PT_LOAD,
     endian::Endianness,
-    read::elf::{ElfFile, ProgramHeader},
+    read::elf::{ElfFile, FileHeader, ProgramHeader},
 };
-use proc_macro2::TokenStream;
-use quote::format_ident;
 
 use sel4_build_env::{get_libsel4_include_dirs, get_with_sel4_prefix_relative_fallback};
-use sel4_config::{sel4_cfg, sel4_cfg_if, sel4_cfg_str, sel4_cfg_usize};
-use sel4_platform_info::PLATFORM_INFO;
+use sel4_config::{sel4_cfg, sel4_cfg_str};
 
 pub const SEL4_KERNEL_ENV: &str = "SEL4_KERNEL";
 
 #[sel4_cfg(WORD_SIZE = "64")]
-type FileHeader = object::elf::FileHeader64<Endianness>;
+type FileHeaderImpl = object::elf::FileHeader64<Endianness>;
 
 #[sel4_cfg(WORD_SIZE = "32")]
-type FileHeader = object::elf::FileHeader32<Endianness>;
+type FileHeaderImpl = object::elf::FileHeader32<Endianness>;
 
 const KERNEL_HEADROOM: u64 = 256 * 1024; // TODO: make configurable
 
 const GRANULE_SIZE: u64 = 4096;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-
     {
         let asm_files = []
             .into_iter()
@@ -59,7 +52,7 @@ fn main() {
 
     let kernel_path = get_with_sel4_prefix_relative_fallback(SEL4_KERNEL_ENV, "bin/kernel.elf");
     let kernel_bytes = fs::read(kernel_path).unwrap();
-    let kernel_elf = ElfFile::<FileHeader, _>::parse(kernel_bytes.as_slice()).unwrap();
+    let kernel_elf = ElfFile::<FileHeaderImpl, _>::parse(kernel_bytes.as_slice()).unwrap();
     let kernel_phys_addr_range = elf_phys_addr_range(&kernel_elf);
 
     let loader_phys_start =
@@ -80,19 +73,13 @@ fn main() {
 
 // // //
 
-fn elf_phys_addr_range<'a, R: ReadRef<'a>>(elf: &ElfFile<'a, FileHeader, R>) -> Range<u64> {
+fn elf_phys_addr_range<'a, R: ReadRef<'a>>(elf: &ElfFile<'a, FileHeaderImpl, R>) -> Range<u64> {
     let endian = elf.endian();
-    let virt_min = elf
-        .elf_program_headers()
-        .iter()
-        .filter(|phdr| phdr.p_type(endian) == PT_LOAD)
+    let virt_min = loadable_segments(elf)
         .map(|phdr| phdr.p_paddr(endian))
         .min()
         .unwrap();
-    let virt_max = elf
-        .elf_program_headers()
-        .iter()
-        .filter(|phdr| phdr.p_type(endian) == PT_LOAD)
+    let virt_max = loadable_segments(elf)
         .map(|phdr| {
             phdr.p_paddr(endian)
                 .checked_add(phdr.p_memsz(endian))
@@ -103,16 +90,10 @@ fn elf_phys_addr_range<'a, R: ReadRef<'a>>(elf: &ElfFile<'a, FileHeader, R>) -> 
     virt_min.into()..virt_max.into()
 }
 
-fn phys_to_virt_offset_for(paddr: u64, vaddr: u64) -> u64 {
-    vaddr.wrapping_sub(paddr)
-}
-
-fn virt_to_phys(vaddr: u64, phys_to_virt_offset: u64) -> u64 {
-    vaddr.wrapping_sub(phys_to_virt_offset)
-}
-
-fn unified<T: Eq>(mut it: impl Iterator<Item = T>) -> T {
-    let first = it.next().unwrap();
-    assert!(it.all(|subsequent| subsequent == first));
-    first
+fn loadable_segments<'data, 'file, R: ReadRef<'data>>(
+    elf: &'file ElfFile<'data, FileHeaderImpl, R>,
+) -> impl Iterator<Item = &'data <FileHeaderImpl as FileHeader>::ProgramHeader> {
+    elf.elf_program_headers()
+        .iter()
+        .filter(|seg| seg.p_type(elf.endian()) == PT_LOAD)
 }
