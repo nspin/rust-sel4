9db0a121f1718e950372dec1f2851ef8178c144f
Nick Spinale <nick@nickspinale.com>
2026-05-22 01:43:33 -0700

    x

--- crates/sel4-kernel-loader/masquerade/image/src/main.rs
+++ crates/sel4-kernel-loader/masquerade/image/src/main.rs
@@ -9,5 +9,18 @@
 #![no_core]
 #![no_std]
 #![no_main]
+#![feature(lang_items)]
+#![allow(internal_features)]
 
 // naked
+//
+// Copyright 2023, Colias Group, LLC
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+
+
+static X: [0; 10] = [0; _];
+
+#[lang = "sized"]
+trait Sized {}
