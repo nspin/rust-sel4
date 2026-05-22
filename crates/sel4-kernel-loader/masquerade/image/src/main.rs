b41bd184f9dc9edc8a18f2f8c1c4a1297a5e6c98
Nick Spinale <nick@nickspinale.com>
2026-05-22 03:42:15 -0700

    x

--- crates/sel4-kernel-loader/masquerade/image/src/main.rs
+++ crates/sel4-kernel-loader/masquerade/image/src/main.rs
@@ -7,12 +7,15 @@
 #![no_std]
 #![no_main]
 
-use core::arch::{naked_asm};
+use core::arch::naked_asm;
 use core::mem;
 use core::ptr;
-use core::slice;
 
 extern crate sel4_no_panic;
+
+mod payload;
+
+use payload::Payload;
 
 // RUSTFLAGS="-Ccode-model=tiny -Cforce-frame-pointers=no -Cforce-unwind-tables=no -Clink-arg=--no-eh-frame-hdr"
 
@@ -57,96 +60,13 @@ unsafe extern "C" fn _start() -> ! {
 }
 
 extern "C" fn rust_entry(dtb_addr: usize) {
-    match main(dtb_addr) {
-        Err(Abort) => (),
-    }
+    main(dtb_addr)
 }
 
-enum Never {}
-
-struct Abort;
-
-#[repr(C)]
-struct BigEndianWord {
-    _align: [usize; 0],
-    bytes: [u8; size_of::<usize>()],
-}
-
-impl BigEndianWord {
-    fn to_usize(&self) -> usize {
-        usize::from_be_bytes(self.bytes)
-    }
-
-    fn to_mut_ptr(&self) -> *mut u8 {
-        self.to_usize() as *mut u8
-    }
-}
-
-type PayloadEntryFn = extern "C" fn(usize) -> !;
-
-struct Payload {
-    entry: usize,
-    regions: &'static [Region],
-    data: *const u8,
-}
-
-impl Payload {
-    unsafe fn deserialize(start: *const u8) -> Self {
-        let mut de = Deserializer::new(start.cast::<BigEndianWord>());
-        let entry = unsafe { de.next() }.to_usize();
-        let num_regions = unsafe { de.next() }.to_usize();
-        let (regions, data) = unsafe { de.rest(num_regions) };
-        Self { entry, regions, data }
-    }
-
-    unsafe fn deploy(&self) -> Result<usize, Abort> {
-        for region in self.regions {
-            unsafe {
-                let src = self.data.add(region.offset.to_usize());
-                let filesz = region.filesz.to_usize();
-                ptr::copy(src, region.vaddr.to_mut_ptr(), region.filesz.to_usize());
-                ptr::write_bytes(region.vaddr.to_mut_ptr().add(filesz), 0, region.memsz.to_usize() - filesz);
-            }
-        }
-        Ok(self.entry)
-    }
-}
-
-#[repr(C)]
-struct Region {
-    vaddr: BigEndianWord,
-    offset: BigEndianWord,
-    filesz: BigEndianWord,
-    memsz: BigEndianWord,
-}
-
-struct Deserializer {
-    cursor: *const BigEndianWord,
-}
-
-impl Deserializer {
-    fn new(start: *const BigEndianWord) -> Self {
-        Self {
-            cursor: start,
-        }
-    }
-
-    unsafe fn next(&mut self) -> BigEndianWord {
-        unsafe {
-            let word = self.cursor.read();
-            self.cursor = self.cursor.add(1);
-            word
-        }
-    }
-
-    unsafe fn rest(self, num_regions: usize) -> (&'static [Region], *const u8) {
-        let p = self.cursor.cast::<Region>();
-        unsafe {
-            let regions = slice::from_raw_parts(p, num_regions);
-            let data = p.add(num_regions).cast::<u8>();
-            (regions, data)
-        }
-    }
+fn main(dtb_addr: usize) -> ! {
+    let entry = unsafe { get_payload().deploy() };
+    let entry = unsafe { mem::transmute::<usize, EntryFn>(entry) };
+    (entry)(dtb_addr)
 }
 
 unsafe extern "C" {
@@ -154,17 +74,7 @@ unsafe extern "C" {
 }
 
 fn get_payload() -> Payload {
-    unsafe {
-        Payload::deserialize(ptr::addr_of!(_payload_start).cast::<u8>())
-    }
+    unsafe { Payload::deserialize(ptr::addr_of!(_payload_start).cast::<u8>()) }
 }
 
-fn main(dtb_addr: usize) -> Result<Never, Abort> {
-    let entry = unsafe {
-        get_payload().deploy()?
-    };
-    let entry = unsafe {
-        mem::transmute::<usize, PayloadEntryFn>(entry)
-    };
-    (entry)(dtb_addr)
-}
+type EntryFn = extern "C" fn(usize) -> !;
---
+++ crates/sel4-kernel-loader/masquerade/image/src/payload.rs
@@ -0,0 +1,95 @@
+//
+// Copyright 2026, Colias Group, LLC
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+
+use core::ptr;
+use core::slice;
+
+#[repr(C)]
+struct BigEndianWord {
+    _align: [usize; 0],
+    bytes: [u8; size_of::<usize>()],
+}
+
+impl BigEndianWord {
+    fn to_usize(&self) -> usize {
+        usize::from_be_bytes(self.bytes)
+    }
+
+    fn to_mut_ptr(&self) -> *mut u8 {
+        self.to_usize() as *mut u8
+    }
+}
+
+pub(crate) struct Payload {
+    entry: usize,
+    regions: &'static [Region],
+    data: *const u8,
+}
+
+#[repr(C)]
+struct Region {
+    vaddr: BigEndianWord,
+    offset: BigEndianWord,
+    filesz: BigEndianWord,
+    memsz: BigEndianWord,
+}
+
+impl Payload {
+    pub(crate) unsafe fn deserialize(start: *const u8) -> Self {
+        let mut de = Deserializer::new(start.cast::<BigEndianWord>());
+        let entry = unsafe { de.next() }.to_usize();
+        let num_regions = unsafe { de.next() }.to_usize();
+        let (regions, data) = unsafe { de.rest(num_regions) };
+        Self {
+            entry,
+            regions,
+            data,
+        }
+    }
+
+    pub(crate) unsafe fn deploy(&self) -> usize {
+        for region in self.regions {
+            unsafe {
+                let src = self.data.add(region.offset.to_usize());
+                let filesz = region.filesz.to_usize();
+                ptr::copy(src, region.vaddr.to_mut_ptr(), region.filesz.to_usize());
+                ptr::write_bytes(
+                    region.vaddr.to_mut_ptr().add(filesz),
+                    0,
+                    region.memsz.to_usize() - filesz,
+                );
+            }
+        }
+        self.entry
+    }
+}
+
+struct Deserializer {
+    cursor: *const BigEndianWord,
+}
+
+impl Deserializer {
+    fn new(start: *const BigEndianWord) -> Self {
+        Self { cursor: start }
+    }
+
+    unsafe fn next(&mut self) -> BigEndianWord {
+        unsafe {
+            let word = self.cursor.read();
+            self.cursor = self.cursor.add(1);
+            word
+        }
+    }
+
+    unsafe fn rest(self, num_regions: usize) -> (&'static [Region], *const u8) {
+        let p = self.cursor.cast::<Region>();
+        unsafe {
+            let regions = slice::from_raw_parts(p, num_regions);
+            let data = p.add(num_regions).cast::<u8>();
+            (regions, data)
+        }
+    }
+}
