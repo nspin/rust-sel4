f1bb583c13e7b979691c4c9603bee9e75e39ec8a
Nick Spinale <nick@nickspinale.com>
2026-05-23 05:04:12 -0700

    x

--- crates/sel4-kernel-loader/masquerade/image/build.rs
+++ crates/sel4-kernel-loader/masquerade/image/build.rs
@@ -9,9 +9,9 @@ use std::path::PathBuf;
 
 fn main() {
     let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    let script = manifest_dir.join("x.lds");
-    println!("cargo::rerun-if-changed={}", script.display());
-    println!("cargo::rustc-link-arg=-T{}", script.display());
+    let linker_script = manifest_dir.join("x.lds");
+    println!("cargo::rerun-if-changed={}", linker_script.display());
+    println!("cargo::rustc-link-arg=-T{}", linker_script.display());
     println!("cargo::rustc-link-arg=--orphan-handling=error");
     println!("cargo::rustc-link-arg=--no-eh-frame-hdr");
     println!("cargo::rustc-link-arg=--gc-sections");
